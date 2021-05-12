2e8564ccecdb92b610878e5e9ec8adc154e390d2
Maksim Brazhnik <brazhnik.m@gmail.com>
2021-05-12 17:53:55 +0300

    add Share to driver documents in reservation

---
+++ precache-manifest.e37641a0374cd000a2af611f76bac8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c183edf634e2f0ff7d2bc0e82a483f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5218d75d7c73270aabd3",
+    "url": "/static/css/2.98ac5457.chunk.css"
+  },
+  {
+    "revision": "3c460ab2d46ff8be28ff",
+    "url": "/static/css/main.3dc36932.chunk.css"
+  },
+  {
+    "revision": "5218d75d7c73270aabd3",
+    "url": "/static/js/2.ee2c1a29.chunk.js"
+  },
+  {
+    "revision": "68d6e6de398477e37631a53ac9d581e4",
+    "url": "/static/js/2.ee2c1a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c460ab2d46ff8be28ff",
+    "url": "/static/js/main.ae2e24e0.chunk.js"
+  },
+  {
+    "revision": "584b8d7b07c75c6d520a",
+    "url": "/static/js/runtime-main.1d6003b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d51ec958d84f9832e3201306bb17e08.js"
+  "/precache-manifest.e37641a0374cd000a2af611f76bac8b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
